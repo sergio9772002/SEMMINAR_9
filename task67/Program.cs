68a2bfc953dcfe0571ceec5d32e921b4d04f5f45
Sergio <sergio977@rambler.ru>
2022-12-04 20:47:18 +0400

    add task67

---
+++ task67/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+int getUserInt(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userInt = Convert.ToInt32(Console.ReadLine());
+    return userInt;
+}
+int UserNumber = getUserInt("Введите число");
+int sumOfDigit = GetSumOfDigit(UserNumber);
+Console.WriteLine($"Сумма цифр числа {UserNumber} равна {sumOfDigit}");
+int GetSumOfDigit(int UserNumber)
+{
+    if (UserNumber == 0)
+    {
+        return 0;
+    }
+    return UserNumber % 10 + GetSumOfDigit(UserNumber / 10);
+}
+
