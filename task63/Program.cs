3937dde51d219ea884d82db60ac34b056e42230a
Sergio <sergio977@rambler.ru>
2022-12-04 20:46:27 +0400

    task63

---
+++ task63/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+*/
+int getUserInt(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userInt = Convert.ToInt32(Console.ReadLine());
+    return userInt;
+}
+int StartIndex = getUserInt("Введите максимальный элемент ряда");
+printRange(StartIndex);
+void printRange(int index)
+{
+    if (index != 0)
+    {
+        printRange(index - 1);
+        Console.Write(index + ",");
+    }
+}
